a0083ac0e10ff8477e129cf04114f59e24e1241b
anirudhrata <anirudh.iist@gmail.com>
2018-12-21 19:36:57 +0530

    Create new getallmessages func

--- webwhatsapi/js/wapi.js
+++ webwhatsapi/js/wapi.js
@@ -1437,3 +1437,21 @@ window.WAPI._MYserializeMessageObj = (obj) => {
     });
 };
 
+
+window.WAPI.MYgetAllMessagesInChat = function (id, includeMe, includeNotifications, done) {
+    const chat = WAPI.getChat(id);
+    let output = [];
+    const messages = chat.msgs.models;
+    for (const i in messages) {
+        if (i === "remove") {
+            continue;
+        }
+        const messageObj = messages[i];
+
+        let message = WAPI.MYprocessMessageObj(messageObj, includeMe, includeNotifications)
+        if (message)
+            output.push(message);
+    }
+    if (done !== undefined) done(output);
+    return output;
+};
